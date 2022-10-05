cbc9d5a232645e40365564b853c5516b9e965155
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-10-05 18:17:19 -0500

    Update HttpHandler.js

--- static/HttpHandler.js
+++ static/HttpHandler.js
@@ -1,1 +1,6 @@
-
+self.addEventListener( "install", function( event ){
+    console.log( "WORKER: install event in progress." );
+});
+self.addEventListener( "activate", event => {
+    console.log('WORKER: activate event in progress.');
+});
