a2bed1b9ca1979bced3d28580094f57524c42d93
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-10-05 11:44:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from base64 import b64decode
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Luanch test"
+@app.route("/tools/pagerender")
+def pageRender():
+    args = request.args
+    if "b64" in args:
+        args = json.loads(b64decode(args.get("b64")))
